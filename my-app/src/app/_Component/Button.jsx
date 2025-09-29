4883cf57d1c8ea99b51f2f69af0a8207c6d4da89
25LP5580 25LP5580 <25LP5580@G303-CC0156.local>
2025-09-29 09:53:05 +0800

    movir-app

---
+++ my-app/src/app/_Component/Button.jsx
@@ -0,0 +1,10 @@
+"use client"
+import React from "react"
+
+export const Button = ({isContinue,text})=>{
+    console.log(isContinue);
+    if (isContinue) {
+
+    }
+    return <div className=" flex justify-center items-end bg-black border-[6px] rounded-2xl text-white">Continue 1/3 </div>
+}
---
+++ my-app/src/app/page.js
@@ -0,0 +1,14 @@
+"use client"
+import {Button} from "./_Component/Button"
+
+export default function Home (){
+    return <div className="p-[32px] bg-white w-[480px] h-[655px] ">
+            <p className="text-3xl text-black-700">Join Us!</p>
+            <p className="text-gray-400 text-[]18px">Please provide all current information accurately.</p>
+
+           <Button isContinue={true} text={"Continue"}/>
+
+      
+        </div>
+    
+}
